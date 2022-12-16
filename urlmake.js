c76cc3cfd39a54434717d2948f7c29e4fa6de781
MD Julhas Uddin <87124734+smjulhaskhan@users.noreply.github.com>
2022-12-16 12:42:00 +0600

    Add files via upload

---
+++ urlmake.js
@@ -0,0 +1,8 @@
+var setting = {
+pengecualianstatus : true,
+pengecualian : ";ok.ru,bdstall.com,fb.com,copyLink(),awesome.top,worldometers.info,diigo.com,alltop.com,slashdot.org,triberr.com,growthhackers.com,whatsapp.com,instagram.com,adfty.biz,dribbble.com,patreon.com,folkd.com,pearltrees,dzone.com,instapaper.com,tracky.com,feedburner.com,gweb.com,public.sitejot.com,mrlaboratory.info,youtube.com,tumblr.com,play.app.goo.gl,stumbleupon.com,blogspot.com,messenger.com,mix.com,digg.com,bufferapp.com,reddit.com,pinterest.com,linkedin.com,youtu.be,m.me,blogger.com,1.bp.blogspot.com,mrlaboratory.com,facebook.com,twitter.com,javascript:;,mailto:";,
+hanyauntukstatus : false,
+hanyauntuk : ";mrlaboratory.com";, 
+//use ::withpath for autolink specific path exampe.com/download/::withpath
+path : ";#?o=";
+};    
